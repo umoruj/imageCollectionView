61862aee4aa63c3111bfb2d243e43c4210487fbe
umoruj <umoru80@gmail.com>
2016-09-23 14:55:48 +0200

    first commmit

--- imageScroll/ViewController.swift
+++ imageScroll/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var mainScrollView: UIScrollView!
+    
+    var imageArray = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mainScrollView.frame = view.frame
+        
+        imageArray = [#imageLiteral(resourceName: "images-of-landscapes-17"), #imageLiteral(resourceName: "images-of-landscapes-19"), #imageLiteral(resourceName: "photographing-landscapes-3e")]
+        
+        for i in 0..<imageArray.count{
+            let imageView = UIImageView()
+            imageView.image = imageArray[i]
+            imageView.contentMode = .scaleAspectFit
+            let xPosition = self.view.frame.width * CGFloat(i)
+            imageView.frame = CGRect(x: xPosition, y: 0, width: self.mainScrollView.frame.width, height: self.mainScrollView.frame.width)
+            
+            mainScrollView.contentSize.width = mainScrollView.frame.width * CGFloat(i + 1)
+            mainScrollView.addSubview(imageView)
+        }
     }
 
     override func didReceiveMemoryWarning() {
